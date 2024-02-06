a95209680d69e2283cd2281a719b764318af6e1d
abhaysamantut <abhay.samant@gmail.com>
2024-02-06 16:59:03 -0600

    updates for 02/06 class

--- bankaccount.py
+++ bankaccount.py
@@ -1,32 +1,70 @@
 # The BankAccount class simulates a bank account.
 
 class BankAccount:
-    
-    # The __init__ method 
-    
-    def __init__(self):
-        self.balance = float(1000.0)
+    """
+    The BankAccount class represents a bank account.
 
-    # The deposit method makes a deposit into the
-    # account.
+    Attributes:
+        __balance (float): The current balance of the account.
+
+    Methods:
+        deposit(amount): Makes a deposit into the account.
+        withdraw(amount): Withdraws an amount from the account.
+        get_balance(): Returns the account balance.
+        set_balance(x): Sets the account balance to the specified value.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the BankAccount class.
+        The initial balance is set to 1000.0.
+        """
+        self.__balance = float(1000.0)
 
     def deposit(self, amount):
-        self.balance += amount
+        """
+        Makes a deposit into the account.
 
-    # The withdraw method withdraws an amount
-    # from the account.
+        Args:
+            amount (float): The amount to be deposited.
+
+        Returns:
+            None
+        """
+        self.__balance += amount
 
     def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
+        """
+        Withdraws an amount from the account.
+
+        Args:
+            amount (float): The amount to be withdrawn.
+
+        Returns:
+            None
+        """
+        if self.__balance >= amount:
+            self.__balance -= amount
         else:
             print('Error: Insufficient funds')
 
-    # The get_balance method returns the
-    # account balance.
-
     def get_balance(self):
-        return self.balance
+        """
+        Returns the account balance.
+
+        Returns:
+            float: The current balance of the account.
+        """
+        return self.__balance
 
     def set_balance(self, x):
-        self.balance=x
+        """
+        Sets the account balance to the specified value.
+
+        Args:
+            x (float): The new balance value.
+
+        Returns:
+            None
+        """
+        self.__balance = x
